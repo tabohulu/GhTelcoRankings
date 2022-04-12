fe8bc2a91a7f66d04216e91b9da20a7cbc5cedde
Kwame Ackah Bohulu <bohulukwame@gmail.com>
2022-04-13 01:45:09 +0900

    move bg job

--- app/routes.py
+++ app/routes.py
@@ -10,24 +10,24 @@ api=rb.create_api()
 queries=["mtnghana","#mtnghana","#vodafoneghana","vodafoneghana","airteltigo","#airteltigo"]
 scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job(IntervalTrigger(minutes=1,timezone="Asia/Tokyo"))
-def print_something():
-    for query in queries:
-        cursor_pos=CursorPosition.get_cursor_position(query)
-        if cursor_pos is None:
-            CursorPosition.create_cursor_position(1,query)
-            since_id=1
-        since_id=CursorPosition.get_since_id(query)   
-        since_id=rb.capture_tweets(since_id,api,query)
-        CursorPosition.edit_since_id(query,since_id)
-
-# @scheduler.scheduled_job(IntervalTrigger(minutes=5))
+# @scheduler.scheduled_job(IntervalTrigger(minutes=1,timezone="Asia/Tokyo"))
 # def print_something():
-#    queue= rq.Queue('rankings-tasks',connection=Redis.from_url('redis://')) 
-#    job=queue.enqueue('app.tasks.get_tweets')
+#     for query in queries:
+#         cursor_pos=CursorPosition.get_cursor_position(query)
+#         if cursor_pos is None:
+#             CursorPosition.create_cursor_position(1,query)
+#             since_id=1
+#         since_id=CursorPosition.get_since_id(query)   
+#         since_id=rb.capture_tweets(since_id,api,query)
+#         CursorPosition.edit_since_id(query,since_id)
+
+# # @scheduler.scheduled_job(IntervalTrigger(minutes=5))
+# # def print_something():
+# #    queue= rq.Queue('rankings-tasks',connection=Redis.from_url('redis://')) 
+# #    job=queue.enqueue('app.tasks.get_tweets')
     
 
-scheduler.start()
+# scheduler.start()
 
 @app.route('/')
 @app.route('/index')
