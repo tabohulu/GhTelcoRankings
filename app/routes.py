a0204c36eefc5769ba4d5d7e2f0faaa43e841c8c
Kwame Ackah Bohulu <bohulukwame@gmail.com>
2022-04-12 11:22:41 +0900

    edited routes

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,25 @@
 from app import app
-from app.models import Tweets
+from app.models import Tweets,CursorPosition
 from flask import render_template
+import app.bots.retwitter_bot as rb
 # from redis import Redis
 # import rq
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+api=rb.create_api()
+queries=["mtnghana","#mtnghana","#vodafoneghana","vodafoneghana","airteltigo","#airteltigo"]
+scheduler = BackgroundScheduler()
 
-# scheduler = BackgroundScheduler()
+@scheduler.scheduled_job(IntervalTrigger(minutes=1,timezone="Asia/Tokyo"))
+def print_something():
+    for query in queries:
+        cursor_pos=CursorPosition.get_cursor_position(query)
+        if cursor_pos is None:
+            CursorPosition.create_cursor_position(1,query)
+            since_id=1
+        since_id=CursorPosition.get_since_id(query)   
+        since_id=rb.capture_tweets(since_id,api,query)
+        CursorPosition.edit_since_id(query,since_id)
 
 # @scheduler.scheduled_job(IntervalTrigger(minutes=5))
 # def print_something():
@@ -14,7 +27,7 @@ from flask import render_template
 #    job=queue.enqueue('app.tasks.get_tweets')
     
 
-# scheduler.start()
+scheduler.start()
 
 @app.route('/')
 @app.route('/index')
